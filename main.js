b535eac284fdf6f74e6abee341f4d0c813c87d2d
Peter Ramsing <peter.ramsing@mac.com>
2017-12-19 12:12:56 -0800

    ES6, cleanup, and re-organization

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
-var ADD = 'add'
+const ADD = 'add'
 
 
-var runningTotal = 0
-var currentValue = 0
+let runningTotal = 0
+let currentValue = 0
 
 
 function appendNumberToCurrent(number) {
-	var stringifyNumber = number.toString()
+	let stringifyNumber = number.toString()
 	currentValue = currentValue + stringifyNumber
 	currentValue = parseInt(currentValue)
 	updateResult(currentValue)
 }
 
-
-function updateResult(result) {
-	$('#js-result').html(result)
-}
-
-function add() {
-	console.log('add')
-	runningTotal = runningTotal + currentValue
-	currentValue = 0
-	equals(ADD)
-}
-
-function subtract() {
-
-}
-
 function equals(intendedAction) {
-	console.log('equal')
 	if (intendedAction === ADD) {
 		runningTotal = runningTotal + currentValue
 	}
 	currentValue = 0
 	updateResult(runningTotal)
+}
+
+function add() {
+	runningTotal = runningTotal + currentValue
+	currentValue = 0
+	equals(ADD)
 }
