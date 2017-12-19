2c2e2816db09fc5d8d2b6f905501de481de3b54c
Peter Ramsing <peter.ramsing@mac.com>
2017-12-19 12:54:40 -0800

    Finishes addition functionality

--- calculator.js
+++ calculator.js
@@ -1,17 +1,36 @@
+const ADD = 'add'
+
+
 module.exports = {
 	currentValue: 0,
 	runningTotal: 0,
+	currentIntention: null,
+
+	clear() {
+		this.currentValue = 0
+		this.runningTotal = 0
+		this.currentIntention = null
+	},
 
 	appendNumberToCurrent(number) {
+		if (number > 9) {
+			throw new TypeError(`Can't append more than one number at a time.`)
+			return
+		}
+
 		this.currentValue = `${this.currentValue}${number}`
 		this.currentValue = parseInt(this.currentValue)
 	},
 
-	add() {
-		return this.runningTotal
+	equal() {
+		if (this.currentIntention === ADD) {
+			this.runningTotal = this.runningTotal + this.currentValue
+		} // TODO: Else
 	},
 
-	equal() {
-		return this.runningTotal
-	}
+	add() {
+		this.currentIntention = ADD
+		this.equal()
+		this.currentValue = 0
+	},
 }
--- test.js
+++ test.js
@@ -2,20 +2,48 @@ import test from 'ava'
 
 const cal = require('./calculator.js')
 
+test.beforeEach(t => {
+	cal.clear()
+})
+
+test.todo('clear')
+
 test('appendNumberToCurrent', t => {
 	cal.appendNumberToCurrent(1)
 	t.is(cal.currentValue, 1)
 	cal.appendNumberToCurrent(3)
 	t.is(cal.currentValue, 13)
+
+	cal.clear()
+	let error = t.throws(() => {
+		cal.appendNumberToCurrent(11)
+	}, TypeError)
 })
 
-test('add basic', t => {
+test('add', t => {
+	cal.appendNumberToCurrent(1)
+	cal.add()
+	t.is(cal.currentIntention, 'add') // TODO: import consts?
+})
+
+test.todo('equal')
+
+test('basic addition', t => {
 	// Basic
+	t.is(cal.runningTotal, 0)
 	cal.appendNumberToCurrent(1)
 	cal.add()
 	cal.appendNumberToCurrent(1)
 	cal.equal()
 	t.is(cal.runningTotal, 2)
+
+	cal.clear()
+	cal.appendNumberToCurrent(1)
+	cal.appendNumberToCurrent(1)
+	cal.add()
+	cal.appendNumberToCurrent(1)
+	cal.equal()
+	t.is(cal.runningTotal, 12)
 })
 
 test('add complex', t => {
